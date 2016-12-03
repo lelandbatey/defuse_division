f2d368f3cb8fb5685d72b26f6a5679fd10c357ca
Leland Batey <lelandbatey@gmail.com>
2016-12-03 14:03:02 -0800

    Move to less-generic main lib folder name

--- server.py
+++ server.py
@@ -4,14 +4,14 @@ from pprint import pprint
 import logging
 import time
 
-from game.server.server import Server
-import game
+from defusedivision.server.server import Server
+import defusedivision
 
 logformat='%(asctime)s:%(levelname)s:%(name)s:%(filename)s:%(lineno)d:%(message)s'
 logging.basicConfig(format=logformat, level=logging.INFO)
 
 srv = Server('127.0.0.1', 44444)
-bout = game.game.Bout(max_players=2, player_constructor=srv.create_player)
+bout = defusedivision.game.Bout(max_players=2, player_constructor=srv.create_player)
 
 while True:
     bout.add_player()
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ import re
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('game/__init__.py', 'rb') as f:
+with open('defusedivision/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
@@ -57,7 +57,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'defusedivision=game.entrypoint:main'
+            'defusedivision=defusedivision.entrypoint:main'
             ]
         }
 )
